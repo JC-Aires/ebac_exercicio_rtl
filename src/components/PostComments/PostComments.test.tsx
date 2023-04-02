17917e015d61b6d5d66ea6a9f234ed2523e34356
JC-Aires <jcaires63@gmail.com>
2023-04-02 10:29:09 -0300

    Add 2 comments insertion tests

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Adicionar 2 comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('texta-comment'), {
+            target: {
+                value: 'Comentário 1',
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comment'));
+
+
+        fireEvent.change(screen.getByTestId('texta-comment'), {
+            target: {
+                value: 'Comentário 2',
+            }
+        });
+        fireEvent.click(screen.getByTestId('button-comment'));
+
+        expect(screen.getAllByTestId('li-comment')).toHaveLength(2);
+    });
+
 });
